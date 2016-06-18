68d42ba9d0c277bf156a4f6ef3c6234ae4f5249f
Gu Zhengxiong <rectigu@gmail.com>
2016-06-18 22:15:27 +0800

    per-file license header: Added GPL3+.

--- lib/lib.h
+++ lib/lib.h
@@ -14,34 +14,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
-# include <linux/module.h>
-# include <linux/kernel.h>
-# include <linux/syscalls.h>
-
-
-MODULE_LICENSE("GPL");
-
-
-int
-init_module(void)
-{
-  printk(KERN_ALERT "%s\n", "Greetings the World!");
-
-  printk(KERN_ALERT "sys_close = %p\n", sys_close);
-  printk(KERN_ALERT "sys_open = %p\n", sys_open);
-  printk(KERN_ALERT "sys_read = %p\n", sys_read);
-  printk(KERN_ALERT "sys_write = %p\n", sys_write);
-
-  return 0;
-}
-
-
-void
-cleanup_module(void)
-{
-  printk(KERN_ALERT "%s\n", "Farewell the World!");
-
-  return;
-}
